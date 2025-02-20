82ebf227fdc8d34e8d4f217b176c94a7851ee084
Hiruna15 <hirunadilmith15@gmail.com>
2025-02-20 11:54:44 +0530

    implement the reception dashboard

--- index.js
+++ index.js
@@ -1,72 +1,3 @@
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-// import {
-//   getAuth,
-//   createUserWithEmailAndPassword,
-//   signInWithEmailAndPassword,
-//   signOut,
-// } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
-// import {
-//   getFirestore,
-//   doc,
-//   setDoc,
-// } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBx5fhNrSL6mHjgak_SpQIKK0DsXXQXUMk",
-//   authDomain: "iotproject-1cfa2.firebaseapp.com",
-//   projectId: "iotproject-1cfa2",
-//   storageBucket: "iotproject-1cfa2.firebasestorage.app",
-//   messagingSenderId: "844629933940",
-//   appId: "1:844629933940:web:e0ef982b5dc6fab3e1f621",
-//   measurementId: "G-QM1KPT6B5T",
-// };
-
-// const app = initializeApp(firebaseConfig);
-// const auth = getAuth(app);
-// const db = getFirestore(app);
-
-// window.register = function () {
-//   const email = document.getElementById("register-email").value;
-//   const password = document.getElementById("register-password").value;
-//   createUserWithEmailAndPassword(auth, email, password)
-//     .then((userCredential) => {
-//       const user = userCredential.user;
-//       alert("Registered successfully!");
-//       setDoc(doc(db, "users", user.uid), {
-//         email: email,
-//         createdAt: new Date(),
-//       });
-//     })
-//     .catch((error) => {
-//       alert(error.message);
-//     });
-// };
-
-// window.login = function () {
-//   const email = document.getElementById("login-email").value;
-//   const password = document.getElementById("login-password").value;
-//   signInWithEmailAndPassword(auth, email, password)
-//     .then(() => {
-//       alert("Logged in successfully!");
-//     })
-//     .catch((error) => {
-//       alert(error.message);
-//     });
-// };
-
-// window.logout = function () {
-//   signOut(auth)
-//     .then(() => {
-//       alert("Logged out successfully!");
-//     })
-//     .catch((error) => {
-//       alert(error.message);
-//     });
-// };
-
-// -------------------------------------------
-
-// Firebase Initialization
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 import {
   getAuth,
@@ -89,7 +20,6 @@ const db = getFirestore(app);
 const registerBtn = document.getElementById("registerBtn");
 const loginBtn = document.getElementById("loginBtn");
 
-// Register Event Listener
 registerBtn.addEventListener("click", async () => {
   const email = document.getElementById("regEmail").value;
   const password = document.getElementById("regPassword").value;
@@ -118,7 +48,6 @@ registerBtn.addEventListener("click", async () => {
   }
 });
 
-// Login Event Listener
 loginBtn.addEventListener("click", async () => {
   const email = document.getElementById("loginEmail").value;
   const password = document.getElementById("loginPassword").value;
@@ -149,7 +78,6 @@ loginBtn.addEventListener("click", async () => {
   }
 });
 
-// Redirect User After Login
 function redirectToDashboard(userType) {
   switch (userType) {
     case "reception":
